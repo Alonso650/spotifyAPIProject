05de3ddd9010212df427fa430546b688437a0e6c
Hector Alonzo-Torres <hectoralonzotorres@Hectors-MacBook-Pro.local>
2024-07-31 20:17:53 -0700

    added more example code with small changes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,38 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'
-
-dotenv.config();
 import { redirectToAuthCodeFlow, getAccessToken, fetchProfile, populateUI } from './auth/spotifyAuth';
 
-const app = express();
-const clientId = "the-client-id";
+dotenv.config();
+//require("dotenv").config({ path: './.env'});
 
+const app = express();
+// Assert that it is a string
+const clientId = process.env.SPOTIFY_CLIENT_ID as string;
+//const params = new URLSearchParams(window.location.search);
+// const code = params.get("code");
 app.use(cors());
 app.use(express.json());
+
+if(!clientId){
+    throw new Error("Spotify Client ID is not set in enviornment variables")
+}
 
 app.get('/auth', async (req, res) => {
     const code = req.query.code as string;
 
     if(!code){
         // Redirect to Spotify Authorization page
+        //redirectToAuthCodeFlow(clientId);
+        //res.redirect('spotify-authorization-url'); // Change to actual spotify URL
         redirectToAuthCodeFlow(clientId);
-        res.redirect('spotify-authorization-url'); // Change to actual spotify URL
     } else {
         try {
             const accessToken = await getAccessToken(clientId, code);
-            //const profile = await fetchProfile(accessToken);
-            //populateUI(profile); // You can replace this with actual data sending
-           // res.json(profile); // Send profile as response for now
+            const profile = await fetchProfile(accessToken);
+            populateUI(profile); // You can replace this with actual data sending
+            res.json(profile); // Send profile as response for now
+            console.log(profile); // Profile data logs to console
         } catch (error) {
             res.status(500).send('An error occured');
         }
